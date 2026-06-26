cf9dd6c70cfa3c7c5d2f777527b6818db07fca85
nancy <nancy@genesiswtech.com>
2026-06-26 20:10:51 +0545

    cd

--- school_assessment/settings.py
+++ school_assessment/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = "django-insecure-l32i0#9xzps^rv&amo-ywh%l9r2#hj^12)&)s5$7#dk4c5wq(s
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    "cas.edutech.com.np",
     "cass-e8cnhjh4dnhndpb6.eastasia-01.azurewebsites.net",
     "localhost",
     "127.0.0.1",
