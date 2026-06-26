8e5e1706e596c3f2fb9abd2f6ea663f5b7616023
nancy <nancy@genesiswtech.com>
2026-06-26 11:55:14 +0545

    Remove collected static files from repo and fixed url

--- assessment/urls.py
+++ assessment/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.shortcuts import redirect
+
+
+# def home(request):
+    # return redirect("/assessment/login/")
+
 
 urlpatterns = [
+    # path("", home),
+    path('', lambda request: redirect('/assessment/login/')),
     # Authentication
     path(
         "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
@@ -21,11 +29,14 @@ urlpatterns = [
     path("manage-terms/", views.manage_terms, name="manage_terms"),
     path("manage-students/", views.manage_students, name="manage_students"),
     path("students/edit/<int:student_id>/", views.edit_student, name="edit_student"),
-    path("students/delete/<int:student_id>/", views.delete_student, name="delete_student"),
+    path(
+        "students/delete/<int:student_id>/", views.delete_student, name="delete_student"
+    ),
     path("manage-subjects/", views.manage_subjects, name="manage_subjects"),
     path("subjects/edit/<int:subject_id>/", views.edit_subject, name="edit_subject"),
-    path("subjects/delete/<int:subject_id>/", views.delete_subject, name="delete_subject"),
-
+    path(
+        "subjects/delete/<int:subject_id>/", views.delete_subject, name="delete_subject"
+    ),
     # Teacher-only
     path("add-assessment/", views.add_assessment, name="add_assessment"),
     # Reports
@@ -41,9 +52,9 @@ urlpatterns = [
         views.class_report_card,
         name="class_report_card",
     ),
-   path(
-    "report-card/pdf/<int:term_id>/<str:grade>/<str:sec>/",
-    views.class_report_card_pdf,
-    name="class_report_card_pdf",
-),
+    path(
+        "report-card/pdf/<int:term_id>/<str:grade>/<str:sec>/",
+        views.class_report_card_pdf,
+        name="class_report_card_pdf",
+    ),
 ]
--- school_assessment/settings.py
+++ school_assessment/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # school_assessment/settings.py
 LOGIN_REDIRECT_URL = "/assessment/dashboard/"
 LOGOUT_REDIRECT_URL = "/assessment/login/"
+
